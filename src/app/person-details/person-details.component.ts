ff707ce56850efdeff1a5afa2813173d06153651
Bojan <bjnpantic@yahoo.com>
2021-05-29 13:51:50 +0200

    switchMap used

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Friend } from '../friend-model';
 import { GroupService } from '../group.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-person-details',
@@ -21,38 +22,37 @@ export class PersonDetailsComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute, private service: GroupService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (param: Params) => {
+    this.friendSub = this.route.params.pipe(
+      switchMap((param: Params) => {
         this.userId = +param.id;
+        return this.service.getPersons();
+      })
+    ).subscribe(
+      (response: Friend[]) => {
+        this.persons = response;
+        this.userSelected = this.persons[this.userId - 1];
 
-        this.friendSub = this.service.getPersons().subscribe(
-          (response: Friend[]) => {
-            this.persons = response;
-            this.userSelected = this.persons[this.userId - 1];
-
-            this.friendsOfFriends = [];
-            for (let id of this.userSelected.friends) {
-              const friend = this.persons.find(p => p.id === id);
-              friend.friends.forEach(element => {
-                if (this.friendsOfFriends.indexOf(element) === -1 && this.userSelected.friends.indexOf(element) === -1) {
-                  this.friendsOfFriends.push(element);
-                }
-              });
+        this.friendsOfFriends = [];
+        for (let id of this.userSelected.friends) {
+          const friend = this.persons.find(p => p.id === id);
+          friend.friends.forEach(element => {
+            if (this.friendsOfFriends.indexOf(element) === -1 && this.userSelected.friends.indexOf(element) === -1) {
+              this.friendsOfFriends.push(element);
             }
+          });
+        }
 
-            this.suggestedFriends = [];
-            if (this.userSelected.friends.length > 1) {
-              this.persons.forEach(person => {
-                if (person.friends.length > 1 && person !== this.userSelected) {
-                  const filteredFriends = person.friends.filter(value => this.userSelected.friends.includes(value));
-                  if (filteredFriends.length > 1) {
-                    this.suggestedFriends.push(person.id);
-                  }
-                }
-              });
+        this.suggestedFriends = [];
+        if (this.userSelected.friends.length > 1) {
+          this.persons.forEach(person => {
+            if (person.friends.length > 1 && person !== this.userSelected) {
+              const filteredFriends = person.friends.filter(value => this.userSelected.friends.includes(value));
+              if (filteredFriends.length > 1) {
+                this.suggestedFriends.push(person.id);
+              }
             }
-          }
-        );
+          });
+        }
       }
     );
   }
@@ -67,3 +67,73 @@ export class PersonDetailsComponent implements OnInit, OnDestroy {
   }
 
 }
+
+// import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { ActivatedRoute, Params } from '@angular/router';
+
+// import { Subscription } from 'rxjs';
+// import { Friend } from '../friend-model';
+// import { GroupService } from '../group.service';
+
+// @Component({
+//   selector: 'app-person-details',
+//   templateUrl: './person-details.component.html',
+//   styleUrls: ['./person-details.component.css']
+// })
+// export class PersonDetailsComponent implements OnInit, OnDestroy {
+//   userId: number;
+//   persons: Friend[];
+//   userSelected: Friend;
+//   friendsOfFriends: number[];
+//   suggestedFriends: number[];
+//   friendSub: Subscription;
+
+//   constructor(private route: ActivatedRoute, private service: GroupService) { }
+
+//   ngOnInit(): void {
+//     this.route.params.subscribe(
+//       (param: Params) => {
+//         this.userId = +param.id;
+
+//         this.friendSub = this.service.getPersons().subscribe(
+//           (response: Friend[]) => {
+//             this.persons = response;
+//             this.userSelected = this.persons[this.userId - 1];
+
+//             this.friendsOfFriends = [];
+//             for (let id of this.userSelected.friends) {
+//               const friend = this.persons.find(p => p.id === id);
+//               friend.friends.forEach(element => {
+//                 if (this.friendsOfFriends.indexOf(element) === -1 && this.userSelected.friends.indexOf(element) === -1) {
+//                   this.friendsOfFriends.push(element);
+//                 }
+//               });
+//             }
+
+//             this.suggestedFriends = [];
+//             if (this.userSelected.friends.length > 1) {
+//               this.persons.forEach(person => {
+//                 if (person.friends.length > 1 && person !== this.userSelected) {
+//                   const filteredFriends = person.friends.filter(value => this.userSelected.friends.includes(value));
+//                   if (filteredFriends.length > 1) {
+//                     this.suggestedFriends.push(person.id);
+//                   }
+//                 }
+//               });
+//             }
+//           }
+//         );
+//       }
+//     );
+//   }
+
+//   ngOnDestroy() {
+//     this.friendSub.unsubscribe();
+//   }
+
+//   getFriendsName(id: number): string {
+//     const friend = this.persons.find(p => p.id === id);
+//     return (friend.firstName + ' ' + friend.surname);
+//   }
+
+// }
